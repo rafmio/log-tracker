dff61c851bbb5e7246af40664a18c0d30f408a1f
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-12 19:53:06 +0300

    Home PC 12.04.2024 19:53

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -1,11 +1,23 @@
 package parser
 
-improt (
+import (
 	"testing"
+	"time"
 )
 
 // Сначала создаем функцию, которая принимает строку файла - запись в журнале
 // логов, парсим ее на структуру или карту
+
+var logEntry struct {
+	Date   time.Time
+	SrcIP  string
+	Len    string
+	Ttl    string
+	Id     string
+	Spt    string
+	Dpt    string
+	Window string
+}
 
 func TestParseLog(t *testing.T) {
 
