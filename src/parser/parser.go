cec6b50931776de9f16a97eff39a140415994c4e
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-12 21:56:43 +0300

    Home PC 12.04.2024 21:56

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -20,16 +20,35 @@ type LogEntry struct {
 
 func ParseLog(log string) (LogEntry, error) {
 
+	result := LogEntry{}
+
 	if log == "" {
 		return LogEntry{}, ErrEmptyString
 	}
 
-	// split the input string into tokens, put them in a slice
-	tokens := strings.Split(log, " ")
+	tokens := strings.Split(log, " ") // split log-entry into slices
 
-	for _, token := tokens {
-		switch token {
-			case
+	// DATE:
+	// dateStr := tokens[:3] // first 3 elements of 'tokens are the date
+	// tokens := tokens[3:] // trim 'tokens' - delete date
+
+	for _, token := range tokens {
+		if strings.HasPrefix(token, "SRC=") {
+			result.SrcIP = strings.TrimPrefix(token, "SRC=")
+		} else if strings.HasPrefix(token, "LEN=") {
+			result.Len = strings.TrimPrefix(token, "LEN=")
+		} else if strings.HasPrefix(token, "TTL=") {
+			result.Ttl = strings.TrimPrefix(token, "TTL=")
+		} else if strings.HasPrefix(token, "ID=") {
+			result.Id = strings.TrimPrefix(token, "ID=")
+		} else if strings.HasPrefix(token, "SPT=") {
+			result.Spt = strings.TrimPrefix(token, "SPT=")
+		} else if strings.HasPrefix(token, "DPT=") {
+			result.Dpt = strings.TrimPrefix(token, "DPT=")
+		} else if strings.HasPrefix(token, "WINDOW=") {
+			result.Window = strings.TrimPrefix(token, "WINDOW=")
 		}
 	}
+
+	return result, nil
 }
--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -8,16 +8,16 @@ import (
 // Сначала создаем функцию, которая принимает строку файла - запись в журнале
 // логов, парсим ее на структуру или карту
 
-type LogEntry struct {
-	// Date   time.Time
-	SrcIP  string
-	Len    string
-	Ttl    string
-	Id     string
-	Spt    string
-	Dpt    string
-	Window string
-}
+// type LogEntry struct {
+// 	// Date   time.Time
+// 	SrcIP  string
+// 	Len    string
+// 	Ttl    string
+// 	Id     string
+// 	Spt    string
+// 	Dpt    string
+// 	Window string
+// }
 
 type tstCase struct {
 	inputStr string
