26631656785ef6f03ca43dda9b942848dde120ae
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-17 23:44:58 +0300

    #5 write test to FindAndSet()

--- src/parser/findFilePosition_test.go
+++ src/parser/findFilePosition_test.go
@@ -23,13 +23,13 @@ func TestFindAndSet(t *testing.T) {
 	}
 
 	// creating a temp directory for temp files to run tests on them
-	tmpDirName := createTempFiles(t)
+	tmpDirName, tmpFileNames := createTempFiles(t)
 	defer os.RemoveAll(tmpDirName) // for delete all temp dirs and files
 
 }
 
 // createTempFiles returns name of directory for further os.RemoveAll(dirname)
-func createTempFiles(t testing.TB) string {
+func createTempFiles(t testing.TB) (string, []string) {
 	t.Helper()
 	// test line for write into temp files
 	line := "Apr 11 23:03:12 localhost kernel: [23604836.487395] [UFW BLOCK] IN=eth0 OUT= MAC=52:54:00:7c:d8:0f:fe:54:00:7c:d8:0f:08:00 SRC=212.192.158.71 DST=194.58.102.129 LEN=44 TOS=0x00 PREC=0x00 TTL=248 ID=54321 PROTO=TCP SPT=44496 DPT=2099 WINDOW=65535 RES=0x00 SYN URGP=0 "
@@ -46,7 +46,14 @@ func createTempFiles(t testing.TB) string {
 		t.Fatalf("getting current dir: %v", err)
 	}
 
-	pathToDirWithLogs := filepath.Join(currentDir, tempDir)
+	fullPathToDirWithLogs := filepath.Join(currentDir, tempDir) // а зачем нам полный путь?
 
-	return tempDir
+	// slice for storing temp file names
+	tmpFiles := make([]string, 0)
+
+	for i := 0; i < 5; i++ {
+		fileName := filepath.Join("ufw.log", ".", strconv.Itoa(i))
+	}
+
+	return tempDir, tmpFiles
 }
