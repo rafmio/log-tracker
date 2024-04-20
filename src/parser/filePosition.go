844219caf441b1079c013b1ac92267ed199cb0e4
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-20 20:57:27 +0300

    #5 FindFP() method is ready

---
+++ src/parser/filePosition.go
@@ -0,0 +1,22 @@
+package parser
+
+import (
+	"io"
+	"os"
+)
+
+type FilePosition struct {
+	filePosition int64
+}
+
+// the method takes a pointer to an open file and finds the current file position
+func (fp *FilePosition) FindFP(file *os.File) error {
+	filePosition, err := file.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return err
+	}
+
+	fp.filePosition = filePosition
+
+	return nil
+}
--- src/parser/findFilePosition_test.go
+++ src/parser/findFilePosition_test.go
@@ -1,54 +1,52 @@
 package parser
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"strconv"
 	"testing"
 )
 
-type StubFilePosition struct {
-	filePosition int
-}
-
 type TstCase struct {
 	name  string
 	f     *os.File
-	fSize int64            // fs.FileInfo.Size() - want
-	fp    StubFilePosition // got
+	fSize int64 // fs.FileInfo.Size() - want
+	// fp    FilePosition
 }
 
-func TestFindAndSet(t *testing.T) {
+func TestFindFP(t *testing.T) {
 
-	// set stub env variable
-	if err := os.Setenv("VARLOGFP", strconv.Itoa(0)); err != nil {
-		t.Fatalf("setting env var VARLOGFP: %v", err)
-	}
+	stubFilePosition := new(FilePosition) // stub new instance for FilePosition
 
-	// slice for storing temp file names
-	tmpFiles := make([]string, 0)
+	tmpFiles := make([]string, 10)             // slice for storing 10 temp file names
+	tmpDirName := createTempFiles(t, tmpFiles) // temp dir for temp files to run tests
+	defer os.RemoveAll(tmpDirName)             // for delete all temp dirs and files
 
-	// creating a temp directory for temp files to run tests on them
-	tmpDirName := createTempFiles(t, tmpFiles)
-	defer os.RemoveAll(tmpDirName) // for delete all temp dirs and files
-
-	tstCases := make([]TstCase, len(tmpFiles))
+	tstCases := make([]TstCase, len(tmpFiles)) // slice for storing test cases
 
 	// fill test cases struct:
 	for i := 0; i < len(tmpFiles); i++ {
-		tstCases[i].name = tmpFiles[i]          // initialize name field
-		tstCases[i].f, _ = os.Open(tmpFiles[i]) // initialize f field (opened *os.File)
-		defer tstCases[i].f.Close()             // defer closing opened file
-		fi, _ := tstCases[i].f.Stat()           // get stat info
-		tstCases[i].fSize = fi.Size()           // initialize fSize field
+		t.Run("test FilePosition.FindFP()", func(t *testing.T) {
+			tstCases[i].name = tmpFiles[i]                   // initialize name field
+			tstCases[i].f, _ = os.Open(tmpFiles[i])          // initialize f field (opened *os.File)
+			defer tstCases[i].f.Close()                      // defer closing opened file
+			settedFP, _ := tstCases[i].f.Seek(0, io.SeekEnd) // set fp for pass in func further finding
+			fi, _ := tstCases[i].f.Stat()                    // get stat info
+			tstCases[i].fSize = fi.Size()                    // initialize fSize field
+			_ = stubFilePosition.FindFP(tstCases[i].f)       // run FindFP() itself
 
+			if stubFilePosition.filePosition != settedFP || stubFilePosition.filePosition != tstCases[i].fSize {
+				t.Errorf("got %d, want %d, want %d", stubFilePosition.filePosition, settedFP, tstCases[i].fSize)
+			}
+		})
 	}
 }
 
 // createTempFiles returns name of directory for further os.RemoveAll(dirname)
 func createTempFiles(t testing.TB, tmpFiles []string) string {
 	t.Helper()
-	// test line for write into temp files
+	// test lines for write into temp files
 	lines := []string{
 		"Apr 11 23:00:15 localhost kernel: [23604659.769285] [UFW BLOCK] IN=eth0 OUT= MAC=52:54:00:7c:d8:0f:fe:54:00:7c:d8:0f:08:00 SRC=91.240.118.248 DST=194.58.102.129 LEN=40 TOS=0x00 PREC=0x00 TTL=250 ID=34638 PROTO=TCP SPT=41605 DPT=63383 WINDOW=1024 RES=0x00 SYN URGP=0 ",
 		"Apr 11 23:00:33 localhost kernel: [23604677.653676] [UFW BLOCK] IN=eth0 OUT= MAC=52:54:00:7c:d8:0f:fe:54:00:7c:d8:0f:08:00 SRC=185.73.125.150 DST=194.58.102.129 LEN=40 TOS=0x00 PREC=0x00 TTL=246 ID=30104 PROTO=TCP SPT=54319 DPT=42274 WINDOW=1024 RES=0x00 SYN URGP=0 ",
@@ -61,20 +59,17 @@ func createTempFiles(t testing.TB, tmpFiles []string) string {
 		"Apr 11 23:02:53 localhost kernel: [23604817.717013] [UFW BLOCK] IN=eth0 OUT= MAC=52:54:00:7c:d8:0f:fe:54:00:7c:d8:0f:08:00 SRC=193.37.69.142 DST=194.58.102.129 LEN=40 TOS=0x00 PREC=0x00 TTL=246 ID=31037 PROTO=TCP SPT=43047 DPT=43580 WINDOW=1024 RES=0x00 SYN URGP=0 ",
 		"Apr 11 23:03:12 localhost kernel: [23604836.487395] [UFW BLOCK] IN=eth0 OUT= MAC=52:54:00:7c:d8:0f:fe:54:00:7c:d8:0f:08:00 SRC=212.192.158.71 DST=194.58.102.129 LEN=44 TOS=0x00 PREC=0x00 TTL=248 ID=54321 PROTO=TCP SPT=44496 DPT=2099 WINDOW=65535 RES=0x00 SYN URGP=0 ",
 	}
-	// create a temp dir to test
+	// create a temp dir for storing temp files to test
 	tempDir, err := os.MkdirTemp(".", "logs")
 	if err != nil {
 		t.Fatalf("creating temp dir: %v", err)
 	}
 
-	// slice for storing temp file names
-	// tmpFiles := make([]string, 0) // try pass it into current func by pointer
-
 	for i := 0; i < len(lines); i++ {
 		// generating filenames
 		fileName := "ufw.log" + "." + strconv.Itoa(i)
 		tmpFile := filepath.Join(tempDir, fileName) // "logs/ufw.log.*"
-		tmpFiles = append(tmpFiles, tmpFile)
+		tmpFiles[i] = tmpFile                       // filling tmpFiles []string slice
 
 		// create len(lens) files, write line there
 		f, err := os.Create(tmpFile)
