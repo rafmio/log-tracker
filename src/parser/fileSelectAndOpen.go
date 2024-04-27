bf639aadb98e82d1d8c95c4205f93bd5179d87de
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-28 00:11:17 +0300

    refactoring SelectAndOpen()

--- src/parser/fileSelectAndOpen.go
+++ src/parser/fileSelectAndOpen.go
@@ -12,7 +12,10 @@ import (
 
 var ErrGetStatInfo = errors.New("can't get file info via Stat()")
 
-// pass target directory via env
+// Searches for files with a name corresponding to the pattern, selects
+// the non-zero and most recent one, and then opens it and returns a pointer
+// to the open file.
+// The target folder is passed to the function from the environment variable
 func SelectAndOpen(directory string) (*os.File, error) {
 	// create variable for storing filenames and time
 	var mapFiles = make(map[string]time.Time)
@@ -23,14 +26,14 @@ func SelectAndOpen(directory string) (*os.File, error) {
 		return nil, err
 	}
 
-	// fill mapFiles, check if file is empty
+	// fills the map with the names of files corresponding to the pattern
 	for _, filename := range files {
-
-		fi, err := os.Stat(filename)
+		fi, err := os.Stat(filename) // getting information about the file
 		if err != nil {
 			return nil, err
 		}
 
+		// exclude archives and empty files
 		if fi.Size() == 0 || strings.Contains(filename, ".gz") {
 			continue
 		}
