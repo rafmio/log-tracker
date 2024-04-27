da2f2cb22ffab43e99eedc3484f5eeab941df721
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-28 00:19:20 +0300

    write comments to FileReader()

--- src/parser/fileReader.go
+++ src/parser/fileReader.go
@@ -7,16 +7,20 @@ import (
 	"os"
 )
 
+// The function takes two arguments: a pointer to an open file and
+// the value of the file position from which to start reading the file.
 func FileReader(file *os.File, filePosition int64) ([]string, error) {
 
 	slsStr := make([]string, 0)
 
+	// setting the file position
 	_, err := file.Seek(filePosition, io.SeekStart)
 	if err != nil {
 		log.Println("os.Seek():", err)
 		return slsStr, err
 	}
 
+	// read the file line by line and write each line separately into a slice
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
