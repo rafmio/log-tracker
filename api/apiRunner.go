f0363e1794b41c4f540d2030b6b5f5321f21970f
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-08-22 23:25:50 +0300

    Home PC 22.08.2024 23:25 connecting to DB for API

--- api/apiRunner.go
+++ api/apiRunner.go
@@ -15,28 +15,18 @@ import (
 const (
 	dbConfigFileName = "db-config.json"
 	port             = ":8082"
-
-	// variables URL parameter names
-	// sourceName = "sourceName"
-	// startDate = "startDate"
-	// endDate = "endDate"
 )
-
-type DateTimeRange struct {
-	StartDate time.Time
-	EndDate   time.Time
-	StartTime time.Time
-	EndTime   time.Time
-}
 
 /*
 fetchHandler() handles incoming HTTP requests.
 
-It performs the following tasks:
-- checks if the HTTP method is 'GET'. If not, it responds with a 405 status code and an error message.
-- parses the form data from the request
-- set headers
-- determines which source (server) received the request: put list of servers in the slice
+the format of the received request (example):
+https://194.58.102.129:5432/fetch?source_name=cute_ganymede&start_date=2024-08-21T14:35&end_date=2024-08-22T11:50
+
+parameter names:
+- sourceName: Name of the source (black_oxygenium of cute_ganymede)
+- start_date: Start date and time (ISO 8601) of the data to fetch
+- end_date: End date and time (ISO 8601) of the data to fetch
 */
 func fetchHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -46,41 +36,31 @@ func fetchHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// parse form
+	// parse form for determine source (exact DB) and further date parsing
 	err := r.ParseForm()
 	if err != nil {
 		http.Error(w, "Error parsing form", http.StatusBadRequest)
 		return
 	}
 
-	// set headers
-	w.Header().Set("Content-Type", "application/json")
-
 	// determine which source (server) received the request
 	// get parameters from Request.Form
-	sourceNamesStr := r.FormValue("sourceName")          // to know which of the two servers to send the request to
+	sourceNamesStr := r.FormValue("source_name")         // to know which of the two servers to send the request to
 	sourceNamesSls := strings.Split(sourceNamesStr, ",") // sourceNamesSls is of type []string
 
 	// slice for storing list of configs of DB connections to servers (sources)
 	dbConfigs := readConfig(dbConfigFileName) // returns map[string]Source
 
 	for _, src := range sourceNamesSls {
-		// log.Println("src:", src) // for debugging
+
+		// if the source exists in the configuration
 		if _, ok := dbConfigs[src]; !ok {
 			http.Error(w, "Source not found", http.StatusNotFound)
 			return
 		}
 
-		// for sector debugging:
-		// srvr := dbConfigs[src]
-		// w.Write([]byte(srvr.Name))
-		// w.Write([]byte("\n"))
-		// w.Write([]byte(srvr.Host))
-		// w.Write([]byte("\n"))
-		// end of debugging sector
-
 		// set connection to DB
-		srcConf := dbConfigs[src] // srcConf if of type Source
+		srcConf := dbConfigs[src] // 'srcConf' if of type Source
 
 		db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 			srcConf.Host,
@@ -92,14 +72,20 @@ func fetchHandler(w http.ResponseWriter, r *http.Request) {
 		))
 
 		if err != nil {
-			// http.Error(w, "Error connecting to DB", http.StatusInternalServerError)
 			http.Error(w, fmt.Sprintf("Error connecting to DB: %v", err), http.StatusInternalServerError)
 			return
 		}
+
+		if err = db.Ping(); err != nil {
+			http.Error(w, fmt.Sprintf("Error pinging DB: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		// close connection to DB after use
 		defer db.Close()
 
-		startDateStr := r.FormValue("startDate")
-		endDateStr := r.FormValue("endDate")
+		startDateStr := r.FormValue("start_date")
+		endDateStr := r.FormValue("end_date")
 
 		// parse dates
 		startDate, err := time.Parse("2006-01-02", startDateStr)
@@ -129,6 +115,10 @@ func fetchHandler(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, "Error scanning row", http.StatusInternalServerError)
 				return
 			}
+
+			// set headers
+			w.Header().Set("Content-Type", "application/json")
+
 			w.Write([]byte(columnValue + "\n"))
 		}
 	}
