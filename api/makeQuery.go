902ad26ab0479da0f39903cd1d4987adb95fd86f
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-10-04 22:04:23 +0300

    logtracker, API, statistics, queries

--- api/makeQuery.go
+++ api/makeQuery.go
@@ -13,6 +13,16 @@ func newGeneralStatQueryParams() *generalStatQueryParams {
 	newGSQP.statIndicators["uniqueIpCount"] = "Unique IP count"
 
 	newGSQP.queryStatIndicators = make(map[string]string)
+	newGSQP.queryStatIndicators["totalNumberEntries"] = `SELECT COUNT(*) FROM lg_tab;`
+	newGSQP.queryStatIndicators["uniqueIpCount"] = `SELECT COUNT(DISTINCT srcip) FROM lg_tab;`
+	newGSQP.queryStatIndicators["entriesPerDay"] = `
+		SELECT AVG(daily_count) AS average_records_per_day
+		FROM (
+    		SELECT COUNT(*) AS daily_count
+    		FROM lg_tab
+    		GROUP BY DATE(tmstmp)
+		) AS daily_counts;
+		 `
 
 	return newGSQP
 }
