3060ce885cddd8aad1e5eb32b57656c5aba66884
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-10-30 22:56:43 +0300

    Home PC 30.10.2024 22:56 prepare for branch

--- api/generalStatHandler.go
+++ api/generalStatHandler.go
@@ -2,30 +2,8 @@ package main
 
 type ltGeneralStats struct {
 	*DBConnections
-
-	// SQL query parameters
-	// statIndicators      map[string]string // names of statistical indicators: map["internalName"]"name for displaying"
-	// queryStatIndicators map[string]string // SQL queries itself: map["internalName"]"SQL query"
-
-	// the result of sending SQL queries to the database
 	queryResults map[string]float64 // map[indicatorName]float64
 }
 
 func (lt *ltGeneralStats) generalStatQueryParams() {
-	// lt.statIndicators = make(map[string]string)
-	// lt.statIndicators["totalNumberEntries"] = "Total number of entries"
-	// lt.statIndicators["uniqueIpCount"] = "Unique IP count"
-	// lt.statIndicators["entriesPerDay"] = "Average number of entries per day"
-
-	// lt.queryStatIndicators = make(map[string]string)
-	// lt.queryStatIndicators["totalNumberEntries"] = `SELECT COUNT(*) FROM lg_tab;`
-	// lt.queryStatIndicators["uniqueIpCount"] = `SELECT COUNT(DISTINCT srcip) FROM lg_tab;`
-	// lt.queryStatIndicators["entriesPerDay"] = `
-	// 	SELECT AVG(daily_count) AS average_records_per_day
-	// 	FROM (
-	// 		SELECT COUNT(*) AS daily_count
-	// 		FROM lg_tab
-	// 		GROUP BY DATE(tmstmp)
-	// 	) AS daily_counts;
-	// 	 `
 }
